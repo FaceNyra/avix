f6433f37a72918771e83aee7c4b547eabd124a7f
FaceNyra <Facenyra@gmail.com>
2026-07-16 15:02:40 +0500

    Delete bot directory

--- bot/routers/__init__.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Dispatcher
-
-from .start import router as start_router
-
-__all__ = ["setup_routers"]
-
-
-def setup_routers(dp: Dispatcher) -> None:
-	dp.include_router(start_router)
